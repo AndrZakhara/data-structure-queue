53c485b7cbfbeff41598a2262341ceedcca2cbf5
Jarosic <jaroslav.rybalka@ukr.net>
2019-01-23 13:10:05 +0200

    fix_tests/clear fix test for method clear

--- src/tests/clear.test.js
+++ src/tests/clear.test.js
@@ -1,23 +1,23 @@
-/* eslint-disable */
 import Stack from '../index.js';
 
 describe('tests for method clear', () => {
-  let stack;
+  let stack = null;
 
   beforeEach(() => {
     stack = new Stack();
     stack.push(1);
     stack.push(2);
     stack.push(3);
-
   });
 
-  test('instance has not own property clear', () => {
-    expect(stack.hasOwnProperty('clear')).toBeFalsy();
-  });
-
-  test('stack length must be 0', () => {
-    stack.clear()
+  test('stack length must be 0 after using this method', () => {
+    expect(stack.length).toBe(3);
+    stack.clear();
     expect(stack.length).toBe(0);
+  });
+
+  test('stack must be empty after using  method clear', () => {
+    stack.clear();
+    expect(stack.isEmpty()).toBeTruthy();
   });
 });
