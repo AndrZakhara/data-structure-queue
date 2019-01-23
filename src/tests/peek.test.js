9f06dfc1f7c18a3ddb2d41b3a051628009244a09
Jarosic <jaroslav.rybalka@ukr.net>
2019-01-23 15:17:06 +0200

    fix_tests/peek fix test for method peek

--- src/tests/peek.test.js
+++ src/tests/peek.test.js
@@ -1,21 +1,23 @@
-/* eslint-disable */
 import Stack from '../index.js';
 
 describe('tests for method peek', () => {
-  let stack;
+  let stack = null;
 
   beforeEach(() => {
     stack = new Stack();
     stack.push(5);
   });
 
-  test('method must show last element', () => {
+  test('method must always show last element', () => {
     expect(stack.peek()).toBe(5);
-
-  });
-
-  test('throw error if array is empty', () => {
-    const emptyStack = new Stack();
-    expect(() => emptyStack.peek()).toThrow(Error);
+    stack.pop();
+    stack.push('element1');
+    stack.push('element2');
+    stack.push('element3');
+    expect(stack.peek()).toBe('element3');
+    stack.pop();
+    expect(stack.peek()).toBe('element2');
+    stack.pop();
+    expect(stack.peek()).toBe('element1');
   });
 });
