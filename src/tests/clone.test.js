5db9c842b38f8b4ee37cad9727dc9d219c28a977
Jarosic <jaroslav.rybalka@ukr.net>
2019-01-23 13:27:37 +0200

    fix_tests/clone fix test for method clone

--- src/tests/clone.test.js
+++ src/tests/clone.test.js
@@ -9,16 +9,14 @@ describe('tests for method clone', () => {
     stack.push(1);
     stack.push(2);
     stack.push(3);
-
   });
 
   test(
-    'should return a new stack where the length is equal to the length of the original stack', 
+    'should return a new stack where the length is equal to the length of the original stack',
     () => {
-    const newStack = stack.clone();
-
-    expect(stack.length === newStack.length).toBeTruthy();
-  });
+      const newStack = stack.clone();
+      expect(newStack.length).toBe(3);
+    });
 
   test('new stack must be Object', () => {
     const newStack = stack.clone();
