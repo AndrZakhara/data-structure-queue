75c45ab0156b73f6148be3a6941e5e3fbadf6756
Jarosic <jaroslav.rybalka@ukr.net>
2019-01-23 14:48:09 +0200

    fix_tests/copyTo fix test for method copyTo

--- src/tests/copyTo.test.js
+++ src/tests/copyTo.test.js
@@ -1,26 +1,34 @@
-/* eslint-disable */
 import Stack from '../index.js';
 
 describe('tests for method copyTo', () => {
-  let stack;
+  let stack = null;
 
   beforeEach(() => {
     stack = new Stack();
-    stack.push(1);
+    stack.push('element3');
+    stack.push('element2');
+    stack.push('element1');
+  });
+
+  test('method must return an array with cloned elements', () => {
+    const array = [];
+    stack.copyTo(array, 0);
+
+    expect(array[0]).toBe('element1');
+    expect(array[1]).toBe('element2');
+    expect(array[2]).toBe('element3');
   });
 
   test('Throw error if one of the arguments is not an Array', () => {
     const notArray = '';
-    
     expect(() => {
-      stack.copyTo(notArray, 1)
+      stack.copyTo(notArray, 1);
     }).toThrow();
   });
 
   test('Throw error if index < 0', () => {
-
     expect(() => {
-      stack.copyTo([1,2,3], -1)
+      stack.copyTo([1, 2, 3], -1);
     }).toThrow();
   });
 });
